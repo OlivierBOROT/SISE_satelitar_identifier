23093c09bf1a92f9e5056701b3f86a5055462051
OlivierBOROTLyon2 <olivier.borot@univ-lyon2.fr>
2026-01-27 10:39:37 +0100

    Add bbox cleaning utility and update .gitignore
    Added utils/clean_overlapping_bboxes.py for cleaning overlapping bounding boxes. Updated .gitignore to exclude cache, __pycache__, and .pyc files. Removed obsolete cache JSON files. Minor updates to notebooks/0_image_scraping.ipynb.

---
+++ utils/clean_overlapping_bboxes.py
@@ -0,0 +1,50 @@
+import geopandas as gpd
+import pandas as pd
+from shapely.geometry import MultiPolygon, Polygon
+
+def clean_overlapping_bboxes(
+    gdf, threshold=0.3, what_to_do_on_overlap="remove_smaller"
+):
+    """
+    Provides functionality to clean overlapping bounding boxes in a GeoDataFrame.
+
+    Args:
+        gdf (gpd.GeoDataFrame): GeoDataFrame containing the bounding boxes.
+        threshold (float, optional): Overlap threshold to consider. Defaults to 0.3.
+        what_to_do_on_overlap (str, optional): Action to take on overlap. Defaults to "remove_smaller". Options are "remove_smaller" or "merge".
+
+    Raises:
+        ValueError: If any geometry in the GeoDataFrame is not a Polygon or MultiPolygon.
+
+    Returns:
+        gpd.GeoDataFrame: Cleaned GeoDataFrame with overlapping bounding boxes handled as specified.
+    """
+
+    for polygon in gdf.geometry:
+        if not isinstance(polygon, (Polygon, MultiPolygon)):
+            raise ValueError("Geometry must be a Polygon or MultiPolygon")
+        for other in gdf.geometry:
+            if polygon == other:
+                continue
+            inter = polygon.intersection(other).area
+            union = polygon.union(other).area
+            iou = inter / union
+            if iou > threshold:
+                if what_to_do_on_overlap == "remove_smaller":
+                    # remove the smaller bbox
+                    if polygon.area >= other.area:
+                        gdf = gdf[gdf.geometry != other]
+                    else:
+                        gdf = gdf[gdf.geometry != polygon]
+                elif what_to_do_on_overlap == "merge":
+                    # calculate the new bbox
+                    new_polygon = polygon.union(other).envelope
+                    # remove the two old ones and add the new one
+                    gdf = gdf[gdf.geometry != polygon]
+                    gdf = gdf[gdf.geometry != other]
+                    gdf = pd.concat(
+                        [gdf, gpd.GeoDataFrame([{"geometry": new_polygon}])],
+                        ignore_index=True,
+                    )
+
+    return gdf
